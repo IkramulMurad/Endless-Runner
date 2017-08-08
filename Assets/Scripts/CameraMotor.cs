2c1e18551974e40ca6dade417569de591c06ba52
Ikramul Murad <ikramul.murad1993@gmail.com>
2017-08-08 20:27:25 +0600

    Camera is following player

---
+++ Assets/Scripts/CameraMotor.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMotor : MonoBehaviour {
+
+	private Transform playerTransform;
+	private Vector3 initialDistance;
+
+	// Use this for initialization
+	void Start () {
+		playerTransform = GameObject.FindWithTag("Player").transform;
+		initialDistance = transform.position - playerTransform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = playerTransform.position + initialDistance;
+	}
+}
