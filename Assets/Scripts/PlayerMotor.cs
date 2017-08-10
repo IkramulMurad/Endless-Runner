f7d8625d0b7e17974683cdaa15007a155ac87337
Ikramul Murad <ikramul.murad1993@gmail.com>
2017-08-10 14:37:41 +0600

    Player movement through x axis is converted to discrete

--- Assets/Scripts/PlayerMotor.cs
+++ Assets/Scripts/PlayerMotor.cs
@@ -13,6 +13,9 @@ public class PlayerMotor : MonoBehaviour {
 	private bool isDead = false;
 	private float startTime;
 
+	private float currentX;
+	private float deltaX;
+
 	private float animationDuration = 3.0f;
 
 	// Use this for initialization
@@ -43,13 +46,33 @@ public class PlayerMotor : MonoBehaviour {
 		}
 
 		//x
-		moveVector.x = Input.GetAxisRaw("Horizontal") * speed;
+		//moveVector.x = Input.GetAxisRaw("Horizontal") * speed;
 		//y
 		moveVector.y = verticalVelocity;
 		//z
 		moveVector.z = speed;
 
 		controller.Move (moveVector * Time.deltaTime);
+
+		//player movement through x axis
+		if (Input.GetKeyDown(KeyCode.RightArrow)) {
+			if (transform.position.x == 2.0f)
+				deltaX = 0;
+			else
+				deltaX = 2.0f;
+
+			transform.position += new Vector3 (deltaX, 0.0f, 0.0f);
+			//Debug.Log(transform.position.x);
+		}
+		if (Input.GetKeyDown(KeyCode.LeftArrow)) {
+			if (transform.position.x == -2.0f)
+				deltaX = 0;
+			else
+				deltaX = -2.0f;
+
+			transform.position += new Vector3 (deltaX, 0.0f, 0.0f);
+			//Debug.Log(transform.position.x);
+		}
 	}
 
 	public void set_speed(int modifier){
