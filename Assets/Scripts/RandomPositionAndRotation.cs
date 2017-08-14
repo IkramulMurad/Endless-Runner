aee13a49d2e7a01eb7435a19707c245f7aa5effd
Ikramul Murad <ikramul.murad1993@gmail.com>
2017-08-14 17:31:18 +0600

    random position and rotation of enemy is implemented

---
+++ Assets/Scripts/RandomPositionAndRotation.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomPositionAndRotation : MonoBehaviour {
+
+	private bool positionChanged = false;
+	private bool rotationChanged = false;
+	private int lastPosIndex = -1;
+	private int lastRotIndex = -1;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(!positionChanged){
+			change_position();
+		}
+		if(!rotationChanged){
+			change_rotation();
+		}
+	}
+
+	private void change_rotation(){
+		float[] yRot = {0f, 26f, 56f, 87f, 130f, 155f, 220f, 293f, 350f};
+		int randomNumber = random_index(0, yRot.Length, lastRotIndex);
+
+		//dynamic
+		// Vector3 originalAngles = transform.rotation.eulerAngles;
+		// transform.rotation = Quaternion.Euler(originalAngles.x, yRot[randomNumber], originalAngles.z);
+
+		transform.rotation = Quaternion.Euler(0f, yRot[randomNumber], 0f);
+		rotationChanged = true;
+
+		// Debug.Log("index: " + randomNumber + "\n" + "val: " + yRot[randomNumber]);
+		// Debug.Log("x: " + transform.rotation.x + "\n" + "y: " + transform.rotation.eulerAngles.y);
+	}
+
+	private void change_position(){
+		int[] xPos = {-2, 0, 2};
+		int randomNumber = random_index(0, xPos.Length, lastPosIndex);
+		Vector3 pos = transform.position;
+
+		pos.x = xPos[randomNumber];
+		transform.position = pos;
+
+		positionChanged = true;
+	}
+
+	private int random_index(int min, int max, int lastIndex){
+		int randomIndex = lastIndex;
+
+		while(randomIndex == lastIndex){
+			randomIndex = Random.Range(min, max);
+		}
+
+		return randomIndex;
+	}
+}
